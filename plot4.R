b1a9fef705b2c4cc29fd9ec12a500997c50e348d
Mariya Mansurova <phystehgirl@gmail.com>
2014-10-07 19:00:48 +0400

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,10 @@
+library("dplyr")
+library("lubridate")
+
+#reading data from file
+data.frame = read.csv("household_power_consumption.txt", sep=";", na.strings= "?")
+
+#processing data
+data = tbl_df(data.frame)
+data = filter(data, Date == '1/2/2007' | Date == '2/2/2007')
+data = mutate(data, datetime = dmy_hms(paste(data$Date, data$Time, sep = " ")))
